de480bbdacc5940bf53cef4352a3904f4547be24
gpspelle <gpsunicamp016@gmail.com>
2019-12-12 16:26:13 +0100

    started to get results: mae = 7

---
+++ calc_mae.py
@@ -0,0 +1,34 @@
+import csv
+
+with open('output.csv', 'r') as t1, open('truth.csv', 'r') as t2:
+    sum = 0
+    count = 0
+    fileone = t1.readlines()
+    filetwo = t2.readlines()
+
+    for row1, row2 in zip(fileone, filetwo):
+        v1 = row1.split(',')
+        v2 = row2.split(',')
+        sum += abs((float(v1[1]) - float(v2[1])))
+        count += 1
+
+    mae = sum / count
+    print(mae)
+
+with open('output.csv', 'r') as t1, open('truth.csv', 'r') as t2:
+    sum = 0
+    count = 0
+    fileone = t1.readlines()
+    filetwo = t2.readlines()
+
+    for row1, row2 in zip(fileone, filetwo):
+        v1 = row1.split(',')
+        v2 = row2.split(',')
+        sum += (float(v1[1]) - float(v2[1])) * (float(v1[1]) - float(v2[1]))
+        count += 1
+
+    rmse = sum ** 0.5
+    rmse = rmse / (count ** 0.5)
+
+    print(rmse)
+
